fc38b3d73013ab66198acb0ab86a3d146ed51b44
joelsteven <joelofelectronics@gmail.com>
2023-09-22 20:39:22 +0300

    Fixed puppet lint

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
 # Create file in /tmp.
 
 file { '/tmp/school':
-  ensure => 'file',
-  mode   => '0744',
-  owner  => 'www-data',
-  group  => 'www-data',
+  ensure  => 'file',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
   content => 'I love Puppet',
 }
